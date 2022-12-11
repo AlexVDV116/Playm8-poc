0976119ae478f0b89032d771046bb9015d76daec
Alex Van der velden <a.vandervelden3@student.avans.nl>
2022-12-11 14:30:28 +0100

    finished class methods

--- Playm8-poc.php
+++ Playm8-poc.php
@@ -23,7 +23,8 @@ class Account {
         $this->userProfile = new userProfile($userProfileID, $firstName, $location, $phoneNumber, $dateOfBirth);
     }
 
-    public function getAccountID() {
+    public function getAccountID() : string 
+    {
         return $this->accountID;
     }
 
@@ -50,7 +51,22 @@ class Account {
         return $this->roles;
     }
 
-    
+    public function checkPermission(Permission $permission) : bool
+    {
+        $found = false;
+        foreach($this->roles as $role) {
+            if($role->checkPermission($permission)) {
+                $found = true;
+                break;
+            }
+        }
+
+        if ($found === false) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
 
 class userProfile {
@@ -71,84 +87,176 @@ class userProfile {
 }
 
 class roleManager {
-    public function giveRole($account, $role) {
+    public function giveRole(Account $account, Role $role) : void
+     {
         $account->addRole($role);
     }   
 
-    public function remRole($account, $role) {
+    public function remRole(Account $account, Role $role) : void 
+    {
         $account->delRole($role);
     }
 
-    public function showRoles($account) {
+    public function showRoles(Account $account) : array 
+    {
        return $account->showRole();
+    }
+
+    public function givePermission(Role $role, Permission $permission) : void
+    {
+        $role->addPermission($permission);
+    }
+
+    public function remPermission(Role $role, Permission $permission) : void 
+    {
+        $role->delPermission($permission);
+    }
+
+    public function showPermissions(Role $role) : array 
+    {
+        return $role->showPermission();
+    }
+
+    public function checkPermissions(Account $account, Permission $permission)
+    {
+        return $account->checkPermission($permission);
     }
 }
 
 class Role {
     private string $roleName;
     private string $roleDescription;
+    private array $permissions;
 
     public function __construct($roleName, $roleDescription) {
         $this->roleName = $roleName;
         $this->roleDescription = $roleDescription;
+        $this->permissions = array();
     }
 
-    public function __toString(){
+    public function __toString() : string 
+    {
         return $this->roleName;
+    }
+
+    public function addPermission(Permission $permission) : void 
+    {
+        array_push($this->permissions, $permission);
+    }
+
+    public function delPermission(Permission $permission) : void 
+    {
+        unset($this->permissions[array_search($permission, $this->permissions)]); 
+    }
+
+    public function showPermission() : array 
+    {
+        return $this->permissions;
+    }
+    
+    public function checkPermission(Permission $permission) : bool
+    {
+        return in_array($permission, $this->permissions);
     }
 }
 
 class Permission {
     private string $permissionName;
+    private string $permissionDescription;
 
-    public function __construct($permissionName) {
+    public function __construct($permissionName, $permissionDescription) {
         $this->permissionName = $permissionName;
+        $this->permissionDescription = $permissionDescription;
     }
 
 }
 
-// --------------------------------------------------------------POC --------------------------------------------------------------
 
+// -------------------------------------------------------------- PROOF OF CONCEPT --------------------------------------------------------------
+
+
+
+// Instantiation of an account object
 $test_account = new Account("test@email.com", "supersecretpassword01!", true, false);
-
-echo "Instantiation of account object:";
+echo "Instantiation of an account object:";
 echo "<pre>";
 var_dump($test_account);
 echo "</pre>";
 
+// Logging in account
 echo "Logged in account:";
 echo "<pre>";
 $test_account->logIn();
 var_dump($test_account);
 echo "</pre>";
 
-
+// Instantiation of user profile object inside the account object
 $test_account->createUserProfile("Alex", "51.58404459919641, 4.797649863611824", "+31637293365", "10/07/1991");
-
-echo "Instantiation of user profile object in account object:";
+echo "Instantiation of user profile object inside the account object:";
 echo "<pre>";
 $test_account->logOut();
 var_dump($test_account);
 echo "</pre>";
 
-
+// Instantiation of the roleManager object and admin Role object and the roleManager object giving the admin role to the test account
 $role_manager = new roleManager();
 $admin = new Role("admin", "administrator description");
 $role_manager->giveRole($test_account, $admin);
-
-echo nl2br("Instantiation of the roleManager and admin role objects\r\nThe roleManager giving the admin role to the test account");
+echo nl2br("Instantiation of the roleManager and admin role objects &\r\nThe roleManager giving the admin role to the test account:");
 echo "<pre>";
 var_dump($test_account);
 echo "</pre>";
 
+// roleManager showRoles method on the test_account
 echo "roleManager showRoles on test_account:";
 echo "<pre>";
 var_dump($role_manager->showRoles($test_account));
 echo "</pre>";
 
-$role_manager->remRole($test_account, $admin);
+// Instantiation of the del_acc_permission object and the roleManager object giving the permission to the admin role
+$del_acc_permission = new Permission("Delete account", "Permission to delete an account");
+$role_manager->givePermission($admin, $del_acc_permission);
+echo nl2br("Instantiation of the del_acc_permission object &\r\nThe roleManager giving the permission to the admin role:");
+echo "<pre>";
+var_dump($admin);
+echo "</pre>";
 
-echo "RoleManager removing admin role from test_account";
+// roleManager object showing the permissions of the admin role
+echo "roleManager showing permissions of admin role:";
+echo "<pre>";
+var_dump($role_manager->showPermissions($admin));
+echo "</pre>";
+
+// test_account with the admin role and del_account_permission
+echo "test_account with the admin role and del_account_permission:";
+echo "<pre>";
+var_dump($test_account);
+echo "</pre>";
+
+// roleManager object checking test_account for del_acc_permission (loops trough the roles array
+//and checks if permission exists in a role, returns true or false)
+echo "roleManager object checking test_account for del_acc_permission (loops trough the roles array and checks if permission exists in a role, ";
+echo "returns true or false):";
+echo "<pre>";
+var_dump($role_manager->checkPermissions($test_account, $del_acc_permission));
+echo "</pre>";
+
+// roleManager removing the del_acc_permission from the admin role
+$role_manager->remPermission($admin, $del_acc_permission);
+echo "RoleManager removing del_acc_permission permission from admin role:";
+echo "<pre>";
+var_dump($admin);
+echo "</pre>";
+
+// roleManager checking for permission again
+echo "roleManager checking for permission again";
+echo "<pre>";
+var_dump($role_manager->checkPermissions($test_account, $del_acc_permission));
+echo "</pre>";
+
+// roleManager removing the admin role form the test_account
+$role_manager->remRole($test_account, $admin);
+echo "RoleManager removing the admin role from test_account:";
 echo "<pre>";
 var_dump($test_account);
 echo "</pre>";
